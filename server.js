d5d4e0e384d8848d8d0d0072019985ada7742a8c
Molnar-Mark <molnar.mark.krisztian.21i@egressy.info>
2026-03-29 14:56:13 +0200

    Server.js

---
+++ server.js
@@ -0,0 +1,54 @@
+
+require("dotenv").config();
+const express = require("express");
+const db = require("./dbhandler");
+const cors = require('cors');
+const User = require("./User");
+const Order = require("./Order");
+const Products = require("./Products");
+const Address = require("./Address");
+const BillingAddress = require("./BillingAddress");
+const Log = require("./Log");
+const Company = require("./Company");
+const server = express();
+const path = require('path');
+
+server.use(cors()); 
+server.use(express.json());
+server.use(express.static("public",{extensions: ["html"] }));
+server.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+server.use((req, res, next) => {
+    console.log(`BEÉRKEZŐ KÉRÉS: ${req.method} ${req.url}`);
+    next(); 
+});
+
+const PORT = process.env.PORT || 3000;
+server.get(['/products', '/products/:brand'], (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'products.html'));
+});
+
+server.use("/", Company);
+server.use("/", User);
+server.use("/", Order);
+server.use("/", Products);
+server.use("/", Address);
+server.use("/", BillingAddress);
+server.use("/", Log);
+server.use((req, res) => {
+    console.log(`404 HIBA: Nem létező oldal: ${req.url}`);
+    res.status(404).redirect('/404');
+});
+db.dbhandler.sync()
+  .then(() => {
+    console.log("-----------------------------------------");
+    server.listen(PORT, () => {
+      console.log(`Szerver állapot: FUT (Port: ${PORT})`);
+      console.log("-----------------------------------------");
+    });
+  })
+  .catch((error) => {
+    console.error("Nem sikerült az adatbázis szinkronizálása!");
+    console.error(error);
+  });
+
+module.exports = server;
